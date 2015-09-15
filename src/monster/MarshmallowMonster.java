aab91f765794970a607db3f32ad402c3326d52f0
ghop1073 <mhop1073@740s-pl-109.local>
2015-09-15 13:01:55 -0600

    change

--- src/monster/MarshmallowMonster.java
+++ src/monster/MarshmallowMonster.java
@@ -15,6 +15,7 @@ public class MarshmallowMonster
 	}
 
 	public MarshmallowMonster(String monsterName, int monsterEyes, int monsterNoses, double monsterLegs, double monsterHair, boolean monsterBellyButton)
+	
 	{
 		this.monsterName = monsterName;
 		this.monsterEyes = monsterEyes;
@@ -25,6 +26,66 @@ public class MarshmallowMonster
 
 	}
 	
+	public String getmonsterName()
+	{
+		return monsterName;
+	}
+	
+	public int getmonsterEyes()
+	{
+		return monsterEyes;
+	}
+	
+	public int getmonsterNoses()
+	{
+		return monsterNoses;
+	}
+	
+	public double getmonsterLegs()
+	{
+		return monsterLegs;
+	}
+	
+	public double getmonsterHair()
+	{
+		return monsterHair;
+	}
+	
+	public boolean getmonsterBellyButton()
+	{
+		return monsterBellyButton;
+	}
+	
+	public void setmonsterName(String monsterName)
+	{
+		this.monsterName = monsterName;
+	}
+	
+	public void setmonsterEyes(int monsterEyes)
+	{
+		this.monsterEyes = monsterEyes;
+	}
+	
+	public void setmonsterNoses()
+	{
+		this.monsterNoses = monsterNoses;
+	}
+	 
+	public void setMonsterLegs()
+	 {
+		 this.monsterLegs = monsterLegs;
+	 }
+	 
+	public void setMonsterHair()
+	 {
+		 this.monsterHair = monsterHair;
+	 }
+	 
+	public void setmonsterBellyButton()
+	 {
+		 this.monsterBellyButton = monsterBellyButton;
+	 }
+	 
 	public String toString()
 	{
 		String monsterInfo = "My monster has " + monsterLegs + " legs and its' name is " + monsterName + ". My monster also has " + monsterEyes + " eyes and " + monsterBellyButton + " belly button.";
